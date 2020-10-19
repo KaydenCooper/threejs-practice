66b0910d30817733460415bc7b5a213ad48af06f
Kayden Cooper <kayden_cooper@yahoo.com>
2020-10-18 21:46:45 -0600

    threejs tutorial

---
+++ src/components/ViewPort.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="viewport" />
+</template>
+
+
+<script>
+import { mapMutations, mapActions } from "vuex";
+export default {
+  data() {
+    return {
+      height: 0,
+    };
+  },
+  mounted() {
+    this.INIT({
+      width: this.$el.offsetWidth,
+      height: this.$el.offsetHeight,
+      el: this.$el,
+    }).then(() => {
+      this.ANIMATE();
+      window.addEventListener("resize", () => {
+        this.RESIZE({
+          width: this.$el.offsetWidth,
+          height: this.$el.offsetHeight,
+        });
+      });
+    });
+  },
+  computed: {},
+  methods: {
+    ...mapMutations(["RESIZE"]),
+    ...mapActions(["INIT", "ANIMATE"]),
+  },
+  components: {},
+};
+</script>
+
+
+<style scoped>
+.viewport {
+  height: 100%;
+  width: 100%;
+}
+</style>
